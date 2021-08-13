45c4f14f3442540bf83e446a8ef68d0fdde22b4a
Sterben667 <83295043+Sterben667@users.noreply.github.com>
2021-08-13 15:19:54 +0200

    Add files via upload

--- fbsearch.py
+++ fbsearch.py
@@ -14,29 +14,27 @@ except :
 discord = "\nDiscord : https://discord.gg/pXc3xeCBVm\n"
 path = os.getcwd()
 mypath = path + "/db"
+shein_path = path +"/db_shein"
 db = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+shein_db = [f for f in listdir(shein_path) if isfile(join(shein_path, f))]
 red = Fore.RED
 blue = Fore.BLUE
 green = Fore.GREEN
 dim = Style.DIM
 reset = Style.RESET_ALL
 title = "Made by Laughing Coffin Team"
-db_count = red + str(len(db)) + reset 
-titled = "\n                                            " + red + title + "\n" + reset 
-nbdb = "                                                  Databases[" + db_count  + "]\n\n\n"
+db_count = len(db) + len(shein_db) 
+titled = "\n      " + red + title + "\n" + reset 
+nbdb = "             Databases[" + red + str(db_count) + reset + "]\n\n\n"
 info = titled + nbdb + discord
 motd = """ 
-
-  █████▒▄▄▄       ▄████▄  ▓█████  ▄▄▄▄    ▒█████   ▒█████   ██ ▄█▀     █████▒██▓ ███▄    █ ▓█████▄ ▓█████  ██▀███     
-▓██   ▒▒████▄    ▒██▀ ▀█  ▓█   ▀ ▓█████▄ ▒██▒  ██▒▒██▒  ██▒ ██▄█▒    ▓██   ▒▓██▒ ██ ▀█   █ ▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒   
-▒████ ░▒██  ▀█▄  ▒▓█    ▄ ▒███   ▒██▒ ▄██▒██░  ██▒▒██░  ██▒▓███▄░    ▒████ ░▒██▒▓██  ▀█ ██▒░██   █▌▒███   ▓██ ░▄█ ▒   
-░▓█▒  ░░██▄▄▄▄██ ▒▓▓▄ ▄██▒▒▓█  ▄ ▒██░█▀  ▒██   ██░▒██   ██░▓██ █▄    ░▓█▒  ░░██░▓██▒  ▐▌██▒░▓█▄   ▌▒▓█  ▄ ▒██▀▀█▄     
-░▒█░    ▓█   ▓██▒▒ ▓███▀ ░░▒████▒░▓█  ▀█▓░ ████▓▒░░ ████▓▒░▒██▒ █▄   ░▒█░   ░██░▒██░   ▓██░░▒████▓ ░▒████▒░██▓ ▒██▒   
- ▒ ░    ▒▒   ▓▒█░░ ░▒ ▒  ░░░ ▒░ ░░▒▓███▀▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒    ▒ ░   ░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░   
- ░       ▒   ▒▒ ░  ░  ▒    ░ ░  ░▒░▒   ░   ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░    ░      ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒  ░ ░  ░  ░▒ ░ ▒░   
- ░ ░     ░   ▒   ░           ░    ░    ░ ░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░     ░ ░    ▒ ░   ░   ░ ░  ░ ░  ░    ░     ░░   ░    
-             ░  ░░ ░         ░  ░ ░          ░ ░      ░ ░  ░  ░              ░           ░    ░       ░  ░   ░        
-                 ░                     ░                                                    ░
+█     ▄█▄        ▄████  ▄█    ▄   ██▄   ▄███▄   █▄▄▄▄ 
+█     █▀ ▀▄      █▀   ▀ ██     █  █  █  █▀   ▀  █  ▄▀ 
+█     █   ▀      █▀▀    ██ ██   █ █   █ ██▄▄    █▀▀▌  
+███▄  █▄  ▄▀     █      ▐█ █ █  █ █  █  █▄   ▄▀ █  █  
+    ▀ ▀███▀       █      ▐ █  █ █ ███▀  ▀███▀     █   
+                   ▀       █   ██                ▀    
+                                                      
                  """
 test_path = path + "//db"
 isdir = os.path.isdir(test_path)
@@ -62,6 +60,29 @@ def results(data, file):
   print(green + "Location: " + red + data[5] + ", " + data[6], end="\n")
   print(reset)
 
+def mailsearch():
+  os.system("clear")
+  print(blue + motd)
+  print(info)
+  mail = input("Mail : ")
+  for files in shein_db:
+    f = open(shein_path + "/" + files, "r", errors='ignore')
+    f1 = f.readlines()
+    for x in f1:
+      x = x.split(":")
+      if x[0] == mail:
+        print(green + dim + "Data Found in : " + files + "!!!!" + reset)
+        print(green + dim + "mail: " + x[0] + " mdp: " + x[1] + reset)
+        input("\n\nPress enter to back to the menu....")
+        f.close()
+        del f, f1
+        main()
+      else:
+        pass
+    print("No data found in :" + files)
+    f.close()
+    del f, f1  
+
 def idsearch():
   os.system("clear")
   print(blue + motd)
@@ -77,11 +98,13 @@ def idsearch():
         results(x, files)
         input("\n\nPress enter to back to the menu....")
         f.close()
+        del f, f1
         main()
       else:
         pass
     print("No data found in :" + files)
     f.close()
+    del f, f1
 
 def phonesearch():
   os.system("clear")
@@ -99,11 +122,13 @@ def phonesearch():
         results(x, files)
         input("\n\nPress enter to back to the menu....")
         f.close()
+        del f, f1
         main()
       else:
         pass
     print("No data found in :" + files)
     f.close()
+    del f, f1
 
 def howtofind():
   os.system("clear")
@@ -125,13 +150,16 @@ def main():
   print(info)
   menu("1", "Search by ID")
   menu("2", "Search by cell number")
+  menu("3", "Search by email")
   menu("3", "How to find Facebook user ID ?")
-  choice = input(red + "---->  ")
-  if choice == "1":
+  choice = int(input(red + "---->  "))
+  if choice == 1:
     idsearch()
-  elif choice == "2":
+  elif choice == 2:
     phonesearch()
-  elif choice == "3":
+  elif choice == 3:
+    mailsearch()
+  elif choice == 3:
     howtofind()
   else:
     input("Please enter a correct option...")
